0c8a1d19135494ab1248dfda5bbe5ba60930423e
ASokolvak <andrey.sokolvak2000@gmail.com>
2022-10-16 13:40:36 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <MainPage/>
+</template>
+
+<script>
+import MainPage from './components/MainPage.vue';
+import './scss/main.scss'
+
+
+export default {
+  name: 'App',
+  components: {
+    MainPage
+}
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,342 @@
+<template>
+  <div class="main">
+
+    <div class="header__top">
+      <div class="container">
+
+
+        <div class="header__top-inner">
+          <a class="logo" href="#">
+            <img class="logo" src="../assets/img/logo.svg" alt="Logo">
+          </a>
+          <nav class="menu">
+            <li class="menu__item">
+              <a class="menu__item-link" href="#about">
+                Про нас
+              </a>
+            </li>
+            <li class="menu__item">
+              <a class="menu__item-link" href="#product">
+                Продукт
+              </a>
+            </li>
+            <li class="menu__item">
+              <a class="menu__item-link" href="#plan">
+                Співпраця
+              </a>
+            </li>
+            <li class="menu__item">
+              <a class="menu__item-link" href="#team">
+                Команда
+              </a>
+            </li>
+            <li class="menu__item">
+              <a class="menu__item-link" href="#contact">
+                Контакти
+              </a>
+            </li>
+          </nav>
+        </div>
+      </div>
+    </div>
+
+    <header class="header">
+      <div class="container">
+
+        <div class="header__content">
+          <h1 class="header__title">
+            Двигун нового покоління
+          </h1>
+          <h3 class="header__subtitle">
+            Стань частиною прогресу та еволюції
+          </h3>
+          <button class="header__btn btn">
+            Цікаво
+          </button>
+        </div>
+      </div>
+    </header>
+    <section class="about" id="about">
+      <div class="container">
+        <h3 class="about__title title">
+          Про нас
+        </h3>
+        <div class="about__inner">
+          <div class="about__inner-item">
+            <img class="about__inner-item-img" src="../assets/img/1.jpg" alt="#">
+            <p class="about__inner-item-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, ratione.
+            </p>
+          </div>
+          <div class="about__inner-item">
+            <img class="about__inner-item-img" src="../assets/img/2.jpg" alt="#">
+            <p class="about__inner-item-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, ratione.
+            </p>
+          </div>
+          <div class="about__inner-item">
+            <img class="about__inner-item-img" src="../assets/img/3.jpg" alt="#">
+            <p class="about__inner-item-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, ratione.
+            </p>
+          </div>
+          <div class="about__inner-item">
+            <img class="about__inner-item-img" src="../assets/img/4.jpg" alt="#">
+            <p class="about__inner-item-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, ratione.
+            </p>
+          </div>
+          <div class="about__inner-item">
+            <img class="about__inner-item-img" src="../assets/img/5.jpg" alt="#">
+            <p class="about__inner-item-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, ratione.
+            </p>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+    <section class="product" id="product">
+      <h3 class="product__title title">
+        Продукт
+      </h3>
+      <div class="product__inner">
+        <img class="product__inner-img" src="../assets/img/engine.jpg" alt="">
+        <div class="product__inner-info">
+          <p class="product__inner-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab cupiditate doloremque tenetur, recusandae
+            harum expedita
+            vel consequatur quisquam sit numquam vero voluptatem aperiam aspernatur amet sed, voluptatum explicabo eum
+            libero.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et vel quas aut harum cumque quod ullam, dicta
+            architecto at
+            nihil, ipsum inventore quisquam nam quia eaque exercitationem? Voluptates, accusantium veritatis.
+          </p>
+          <p class="product__inner-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ipsam nulla, laborum atque, sit
+            ducimus minus aut
+            illo, adipisci magnam dolor optio eveniet? Provident fuga ex nemo repellendus facere veritatis.
+          </p>
+          <p class="product__inner-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ipsam nulla, laborum atque, sit
+            ducimus minus aut
+            illo, adipisci magnam dolor optio eveniet? Provident fuga ex nemo repellendus facere veritatis.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum aspernatur placeat eveniet aliquam,
+            repellendus porro
+            velit cupiditate repellat illum perspiciatis. Soluta non laborum delectus, sunt sed aut facilis. In,
+            earum?
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <section class="plan" id="plan">
+      <div class="container">
+        <h3 class="plan__title title">
+          перспективи
+        </h3>
+        <div class="plan__inner">
+          <div class="plan__items">
+            <div class="plan__item">
+              <h5 class="plan__item-title">
+                Перспектива №1
+              </h5>
+              <p class="plan__item-text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam beatae facere facilis consequuntur animi
+                sequi repellat corporis quaerat reprehenderit optio, tempore corrupti, cupiditate blanditiis illum
+                adipisci molestias omnis eius? Quae!
+              </p>
+            </div>
+            <div class="plan__item">
+              <h5 class="plan__item-title">
+                Перспектива №2
+              </h5>
+              <p class="plan__item-text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam beatae facere facilis consequuntur animi
+                sequi repellat corporis quaerat reprehenderit optio
+              </p>
+            </div>
+            <div class="plan__item">
+              <h5 class="plan__item-title">
+                Перспектива №3
+              </h5>
+              <p class="plan__item-text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam beatae facere facilis consequuntur animi
+                sequi repellat corporis quaerat reprehenderit optio, tempore corrupti, cupiditate blanditiis illum
+                adipisci molestias omnis eius? Quae!
+              </p>
+            </div>
+            <div class="plan__item">
+              <h5 class="plan__item-title">
+                Перспектива №4
+              </h5>
+              <p class="plan__item-text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam beatae facere facilis consequuntur animi
+                sequi repellat corporis quaerat reprehenderit
+              </p>
+            </div>
+            <div class="plan__item">
+              <h5 class="plan__item-title">
+                Перспектива №5
+              </h5>
+              <p class="plan__item-text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam beatae facere facilis consequuntur animi
+                sequi repellat
+              </p>
+            </div>
+            <div class="plan__item">
+              <h5 class="plan__item-title">
+                Перспектива №6
+              </h5>
+              <p class="plan__item-text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam beatae facere facilis consequuntur animi
+                sequi repellat corporis quaerat reprehenderit optio, tempore corrupti, cupiditate blanditiis illum
+                adipisci molestias omnis eius? Quae!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="partner">
+      <div class="container">
+        <h3 class="partner__title title">
+          Партнерство
+        </h3>
+        <div class="partner__inner">
+          <div class="partner__items">
+            <div class="partner__item">
+              <img class="partner__item-img" src="../assets/img/check1.svg" alt="">
+              <p class="partner__item-text">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet illo eveniet deserunt, tempore quam magni
+                nihil architecto alias voluptatibus ratione perspiciatis quod nam. Quae qui, ab a ex at ipsum?
+              </p>
+            </div>
+            <div class="partner__item">
+              <img class="partner__item-img" src="../assets/img/check1.svg" alt="">
+              <p class="partner__item-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, quia?
+              </p>
+            </div>
+            <div class="partner__item">
+              <img class="partner__item-img" src="../assets/img/check1.svg" alt="">
+              <p class="partner__item-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt molestiae cupiditate quisquam ipsa
+                mollitia iusto, neque eum saepe. Et ducimus esse distinctio blanditiis enim culpa ex temporibus quae
+                aspernatur ab!
+              </p>
+            </div>
+            <div class="partner__item">
+              <img class="partner__item-img" src="../assets/img/check1.svg" alt="">
+              <p class="partner__item-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, quia?
+              </p>
+            </div>
+            <div class="partner__item">
+              <img class="partner__item-img" src="../assets/img/check1.svg" alt="">
+              <p class="partner__item-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, quia?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <div class="team" id="team">
+      <div class="container">
+        <h3 class="team__title title">
+          Команда
+        </h3>
+        <div class="team__inner">
+          <div class="team__items">
+            <div class="team__item">
+              <img class="team__item-img" src="../assets/img/team/1.jpg" alt="team">
+              <p class="team__item-name">
+                Дмитро
+              </p>
+              <p class="team__item-pos">
+                CEO-prodaction
+              </p>
+            </div>
+            <div class="team__item">
+              <img class="team__item-img" src="../assets/img/team/2.jpg" alt="team">
+              <p class="team__item-name">
+                Ім'я
+              </p>
+              <p class="team__item-pos">
+                Посада
+              </p>
+            </div>
+            <div class="team__item">
+              <img class="team__item-img" src="../assets/img/team/2.jpg" alt="team">
+              <p class="team__item-name">
+                Ім'я
+              </p>
+              <p class="team__item-pos">
+                Посада
+              </p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+    <h3 class="form__title title">
+      Звязатись з нами
+    </h3>
+    <div class="call" id="contact">
+      <div class="container">
+        <form action="" class="form">
+          <input class="input__name" type="text" placeholder="Ім'я*">
+          <input class="input__tel" type="text" placeholder="Телефон*">
+          <input class="input__email" type="text" placeholder="email*">
+          <textarea class="input__area" name="" id="" cols="20" rows="8"
+            placeholder="Ваше запитання, або пропозиція"></textarea>
+        </form>
+      </div>
+    </div>
+
+
+    <footer class="footer">
+      <div class="container">
+        <div class="footer__inner">
+          <div class="footer__items">
+            <a class="footer__logo" href="">
+              <img  src="../assets/img/logo.svg" alt="">
+            </a>
+            <div class="footer__contact">
+              <a class="phone" href="tel:+380967430568">+ 380 (96) 743 0568</a>
+            </div>
+            <div class="footer__social">
+              <a class="footer__social-link" href="#">
+                <img class="social-icon" src="../assets/img/social/instagram.svg" alt="img">
+              </a>
+              <a class="footer__social-link" href="#">
+                <img class="social-icon" src="../assets/img/social/facebook.svg" alt="img">
+              </a>
+              <a class="footer__social-link" href="#">
+                <img class="social-icon" src="../assets/img/social/youtube.svg" alt="img">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+  props: {
+
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
